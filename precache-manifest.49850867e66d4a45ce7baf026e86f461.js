74326e55d132a341e8909ad670a153241d1702a7
Nicholas Allen <Nick@asherahs-imac.lan>
2019-07-09 15:17:28 -0400

    Updates

---
+++ precache-manifest.49850867e66d4a45ce7baf026e86f461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1b951484d5a4da5467ada1784435a3",
+    "url": "/nickandshera/index.html"
+  },
+  {
+    "revision": "c297585b5c7177362dc8",
+    "url": "/nickandshera/static/css/main.fc74fe3a.chunk.css"
+  },
+  {
+    "revision": "4d5ab6926f9070b5ab13",
+    "url": "/nickandshera/static/js/2.d36305e1.chunk.js"
+  },
+  {
+    "revision": "c297585b5c7177362dc8",
+    "url": "/nickandshera/static/js/main.20af77cb.chunk.js"
+  },
+  {
+    "revision": "5d7540c1bb667eff54b7",
+    "url": "/nickandshera/static/js/runtime~main.a6dd6430.js"
+  }
+]);
